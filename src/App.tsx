147877b7e8bad31d669a3cd23c73b503919b85f7
Arsen Chernov <arsenchernov02@gmail.com>
2024-11-18 20:33:48 +0600

    Added AppToolbar, store and hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import { Container } from "@mui/material";
+import AppToolbar from "./components/AppToolbar/AppToolbar";
+import { Route, Routes } from "react-router-dom";
+
 const App = () => {
   return (
     <>
+      <header>
+        <AppToolbar />
+      </header>
+      <main>
+        <Container maxWidth="xl">
+          <Routes>
+            <Route path="/" element />
+          </Routes>
+        </Container>
+      </main>
     </>
   );
 };
---
+++ src/components/AppToolbar/AppToolbar.tsx
@@ -0,0 +1,24 @@
+import {AppBar, styled, Toolbar, Typography} from '@mui/material';
+import { NavLink } from 'react-router-dom';
+
+const LogoLink = styled(NavLink)({
+    color: 'inherit',
+    textDecoration: 'none',
+    '&:hover': {
+        color: 'inherit',
+    }
+});
+
+const AppToolbar = () => {
+    return (
+        <AppBar position="sticky" sx={{mb: 2}}>
+            <Toolbar>
+                <Typography variant="h6" component="div">
+                    <LogoLink to="/">Weather Today!</LogoLink>
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default AppToolbar;
